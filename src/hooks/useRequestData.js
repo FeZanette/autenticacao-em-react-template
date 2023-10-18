011d8234b2c265c56aa3f58499cf2a52267cabd3
FeZanette <fernandazanette1975@gmail.com>
2023-10-18 15:46:19 -0300

    Aula Autenticação em React template finalizada

--- src/hooks/useRequestData.js
+++ src/hooks/useRequestData.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { BASE_URL } from '../constants/BASE_URL'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/BASE_URL";
+import axios from "axios";
 
+// PRÁTICA 2:
+// 1) Passar o headers como parâmetro na função useRequestData
+// 2) No axios, além da URL também tem que passar o headers de autorização
+// 3) Na página Feed.js, passar o parâmetro headers como outro argumento. Cria o objeto config e chama o headers dentro com a authorization token. Cria a variável token que pega o token do localStorage. Por fim, chama o config no [posts]
 
+export default function useRequestData(estadoInicial, path, headers) {
+  const [dados, setDados] = useState(estadoInicial);
+  const [erro, setErro] = useState("");
 
-export default function useRequestData(estadoInicial, path) {
+  const receberDados = () => {
+    axios
+      .get(`${BASE_URL}${path}`, headers)
+      .then((resposta) => {
+        setDados(resposta.data);
+      })
+      .catch((erro) => {
+        console.log(erro.response);
+        setErro(erro.response);
+      });
+  };
 
-    const [dados, setDados] = useState(estadoInicial)
-    const [erro, setErro] = useState('')
+  useEffect(() => {
+    receberDados();
+  }, [path]);
 
-    const receberDados = () =>{
-        axios.get(`${BASE_URL}${path}`)
-        .then((resposta) => {
-            setDados(resposta.data)
-        })
-        .catch((erro) => {
-            console.log(erro.response)
-            setErro(erro.response)
-        })
-    }
-    
-    useEffect(() => {
-        receberDados()
-    }, [path])
-
-    return [dados, receberDados, erro]
+  return [dados, receberDados, erro];
 }
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CardPost from '../../components/CardPost/CardPost'
 import CriarPost from '../../components/CriarPost/CriarPost'
 import useRequestData from '../../hooks/useRequestData'
 import { FeedContainer } from './styled'
+import { irParaLogin } from '../../routes/coordinator'
+import { useNavigate } from 'react-router-dom'
+import useProtectedPage from '../../hooks/useProtectedPage'
 
+// PRÁTICA 2:
+// 1) O parâmetro headers do useRequestData deve ser passado como argumento no [posts]:
+// - Criar o objeto config e chamar o headers dentro com a authorization token.
+// - Criar a variável token que pega o token do localStorage. 
+// - Chamar o config no [posts]
+
+// PRÁTICA 3:
+// 1) Verificar se o token existe através do hook useEffect.Se o token não existir, a página é direcionada para a página de Login através do navigate
+
+// EXERCÍCIO DE FIXAÇÃO:
+// 1) Chamar a função useProtectedPage() e apagar o useEffect
 
 export default function Feed() {
-  const [posts] = useRequestData([], '/posts')
+  const navigate = useNavigate ()
+  const token = localStorage.getItem("token")
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  }
+  const [posts] = useRequestData([], '/posts', config)
+
+  // useEffect (() => {
+  //   if (!token) {
+  //     irParaLogin(navigate)
+  //   }
+  // }, [navigate])
+
+  useProtectedPage ()
 
   return (
     <FeedContainer>
